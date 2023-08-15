446d7bcd2499e83d4c9d2f8d5c2bc4a40c37ffb7
hassanShakur <hass.shakurhassan@gmail.com>
2023-08-15 13:31:50 +0300

    add species, data & transaction 1

--- data.sql
+++ data.sql
@@ -3,3 +3,12 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
     ('Gabumon', '2018-11-15', 8.0, '1', 2),
     ('Pikachu', '2021-1-7', 15.04, '0', 1),
     ('Devimon', '2017-5-12', 11, '1', 5);
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES
+    ('Charmander', '2020-2-8', 11.0, '0', 0),
+    ('Plantmon', '2021-11-15', 5.7, '1', 2),
+    ('Squirtle', '1993-4-2', 12.13, '0', 3),
+    ('Angemon', '2005-6-12', 45.0, '1', 1),
+    ('Boarmon', '2005-6-7', 20.4, '1', 7),
+    ('Blossom', '1998-10-13', 17.0, '1', 3),
+    ('Ditto', '2022-5-14', 22.0, '1', 4);
--- queries.sql
+++ queries.sql
@@ -6,3 +6,10 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = '1';
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+COMMIT;
