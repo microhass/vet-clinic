ef952e91f1d49b5129e74cda1d471e6cf399650a
hassanShakur <hass.shakurhassan@gmail.com>
2023-08-16 13:48:05 +0300

    add vets data

--- schema.sql
+++ schema.sql
@@ -56,3 +56,20 @@ CREATE TABLE vets (
   date_of_graduation DATE,
   PRIMARY KEY (id)
 );
+
+
+-- Specializations join table
+CREATE TABLE specializations (
+  vet_id INT REFERENCES vets(id),
+  species_id INT REFERENCES species(id),
+  PRIMARY KEY (vet_id, species_id)
+);
+
+
+-- Visits join table
+CREATE TABLE visits (
+  vet_id INT REFERENCES vets(id),
+  animal_id INT REFERENCES animals(id),
+  visit_date DATE,
+  PRIMARY KEY(vet_id, animal_id)
+);
