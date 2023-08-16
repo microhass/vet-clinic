54be7f257718ac90415bbd5c94ff1ec1cce6bf88
hassanShakur <hass.shakurhassan@gmail.com>
2023-08-16 08:49:47 +0300

    add data & schema

--- data.sql
+++ data.sql
@@ -4,6 +4,7 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
     ('Pikachu', '2021-1-7', 15.04, '0', 1),
     ('Devimon', '2017-5-12', 11, '1', 5);
 
+
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES
     ('Charmander', '2020-2-8', 11.0, '0', 0),
     ('Plantmon', '2021-11-15', 5.7, '1', 2),
@@ -12,3 +13,38 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
     ('Boarmon', '2005-6-7', 20.4, '1', 7),
     ('Blossom', '1998-10-13', 17.0, '1', 3),
     ('Ditto', '2022-5-14', 22.0, '1', 4);
+
+
+INSERT INTO owners (full_name, age) VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+
+INSERT INTO species (name) VALUES
+    ('Pokemon'),
+    ('Digimon');
+
+
+-- Link animals to species
+BEGIN;
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species_id = 1
+WHERE species_id IS NULL;
+COMMIT;
+
+
+-- Link animals to owners
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+
--- schema.sql
+++ schema.sql
@@ -13,3 +13,36 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species varchar(100);
+
+
+-- Create owners table
+CREATE TABLE owners (
+  id  INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(100),
+  age INT,
+  PRIMARY KEY (id)
+);
+
+
+-- Create species table
+CREATE TABLE species (
+  id  INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
+
+-- Remove species col in animals
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add col species-id to animals
+ALTER TABLE animals ADD COLUMN species_id INT;
+
+-- Make species-id foreign key
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
+
+-- Add col owner-id to animals
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+-- Make owner-id foreign key
+ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES owners(id);
